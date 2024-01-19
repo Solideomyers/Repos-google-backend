3f52a59555822dafdf6f0a49f05a953a5558853d
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-19 10:58:59 -0400

    feat: definicion del dto para los repo segregando la info

---
+++ src/domain/dtos/repo.dto.ts
@@ -0,0 +1,29 @@
+export class RepoDTO {
+  constructor(
+    public id: number,
+    public node_id: string,
+    public name: string,
+    public full_name: string,
+    public privates: boolean,
+    public owner: {
+      login: string;
+      id: number;
+      node_id: string;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      site_admin: boolean;
+    }
+  ) {}
+}
