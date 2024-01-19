585d419faf3b5d93a53e113a9254109d3fbb5732
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-01-19 10:56:42 -0400

    feat: definicion de un error personalizado

---
+++ src/domain/errors/custom.error.ts
@@ -0,0 +1,22 @@
+export class CustomError extends Error {
+  constructor(
+    public readonly statusCode: number,
+    public readonly message: string
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+
+  static badRequest(message: string) {
+    return new CustomError(400, message);
+  }
+  static forbidden(message: string) {
+    return new CustomError(403, message);
+  }
+  static notFound(message: string) {
+    return new CustomError(404, message);
+  }
+  static internalServer(message: string) {
+    return new CustomError(500, message);
+  }
+}
